98e41d63a1050364e7e7a2a73d6c5976403e4382
Ramdularey1 <ramdulareyyadav2@gmail.com>
2024-06-26 22:36:33 +0530

    update image

--- frontend/src/Component/EnglishSongs.jsx
+++ frontend/src/Component/EnglishSongs.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import PlayingMusic from './PlayingMusic';
 import Music from './Music';
 import { updateCurrentSong } from '../utils/currentMusicSlice';
+import image from "../../public/hindi.jpeg"
 
 
 
@@ -39,7 +40,7 @@ console.log("redux",oldSongs)
 
         <div className=' w-10/12  '>
           <div className='w-[200px] flex relative'>
-            <img className='w-[120px]  h-auto sm:w-[200px] md:w-[250px] lg:w-[300px] xl:w-[400px]' src={"./public/hindi.jpeg"} alt="noimage" />
+            <img className='w-[120px]  h-auto sm:w-[200px] md:w-[250px] lg:w-[300px] xl:w-[400px]' src={"../../public/hindi.jpeg"} alt="noimage" />
             <div className='flex items-center flex-col'>
               <h1 className=' p-4 md:w-[200px] text-[22px] lg:w-[250px]'>Trending in Hindi</h1>
               <button className='absolute bottom-0 w-[90px] h-[30px] rounded bg-slate-200'>Play Song</button>
